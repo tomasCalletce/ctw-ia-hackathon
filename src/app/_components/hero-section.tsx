58aecb8c66192a4af6b39b979f347305b17cbdc2
tomas <64665732+tomasCalletce@users.noreply.github.com>
2025-07-20 21:05:54 -0500

    feat: correct typo

--- src/app/_components/hero-section.tsx
+++ src/app/_components/hero-section.tsx
@@ -58,7 +58,7 @@ export function HeroSection() {
               >
                 La hackathon más grande de{" "}
                 <span className="font-medium" style={{ color: "#D9D9D9" }}>
-                  Colombia !!!
+                  Colombia!!!
                 </span>
               </p>
               <ApplyNow />
